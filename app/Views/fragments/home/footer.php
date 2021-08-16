47de0c78f4b62fd1f69bd299bf5a04c6d356d027
Harley Lumagui <lumagui.harl0723@gmail.com>
2021-08-16 14:50:20 +0800

    Removed Spaces on Footer Bottom Margin

--- app/Views/fragments/home/footer.php
+++ app/Views/fragments/home/footer.php
@@ -1,5 +1,5 @@
-    <div class="d-flex flex-wrap justify-content-center align-items-center py-5 my-4 bg-dark">
+    <div class="d-flex flex-wrap justify-content-center align-items-center bg-dark">
         <div class="container">
-            <p class="text-white text-center">&copy; <?= date('Y') ?> SM Supermalls</p>
+            <p class="text-white text-center mt-3">&copy; <?= date('Y') ?> SM Supermalls</p>
         </div>
     </div>
