c31bd579c63cd520b4c602264198043bc181cdf4
Harley Lumagui <lumagui.harl0723@gmail.com>
2021-08-20 14:18:13 +0800

    Apply All Updates from QA

--- app/Views/fragments/home/header.php
+++ app/Views/fragments/home/header.php
@@ -1,12 +1,18 @@
+    <header class="py-3 mb-3">
+        <div class="container container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+            <div class="dropdown1">
+                <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none">
+                    <img src="<?= base_url('images/v2/Logo.png') ?>" width="80px">
+                </a>
+            </div>
 
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-        <div class="container d-flex flex-wrap justify-content-center">
-            <a href="<?= base_url() ?>" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-decoration-none">
-                <img src="<?= base_url('images/v2/Logo.png') ?>" width="100px">
-            </a>
-            <div class="col-md-3">
-                <a href="#how-to-join" class="btn btn-link me-2 text-decoration-none text-white" role="button">How to Join</a>
-                <button type="button" data-bs-toggle="modal" data-bs-target="#signin-modal" class="btn btn-sm-primary shadow">Sign In / Sign Up</button>
+            <div class="d-flex">
+
+                <a href="#how-to-join" class="w-100 btn btn-link me-2 text-decoration-none text-end text-white" role="button">How to Join</a>
+
+                <div class="flex-shrink-0">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#signin-modal" class="btn btn-sm-primary shadow">Sign In / Sign Up</button>
+                </div>
             </div>
         </div>
     </header>
--- app/Views/pages/home/maintenance.php
+++ app/Views/pages/home/maintenance.php
@@ -6,7 +6,7 @@
                 <div class="text-center maintenance">
                     <img class="col-sm-1 col-md-5 col-lg-5" src="<?= base_url('images/v2/APPROVED-SM-SUPER-TIKTOK-CHALLENGE.png') ?>" width="100%">
                 </div>
-                <h5 class="sm-text-yellow text-center mt-4 display-6 fw-bold">
+                <h5 class="sm-text-yellow text-center text-shadow mt-4 display-6 fw-bold">
                     <strong>
                         JOIN THE BIGGEST NATIONWIDE TIKTOK CHALLENGE!
                     </strong>
@@ -16,18 +16,26 @@
                         Create a TikTok video dancing or singing along to the SM Jungle with your fam, friends, or fur babies for a chance to win a super aweSM prizes!
                     </strong>
                 </h6>
-                <h5 class="text-white text-center mt-4">
+                <h5 class="text-white text-center text-shadow mt-4">
                     <strong>
                         Submission of entries is from Aug 30 to Sept 30, 2021.
                     </strong>
                 </h5>
+                <div class="justify-content-center mt-4 text-center">
+                    <a href="#" class="btn btn-sm-primary col-sm-12 col-md-4 col-lg-4">Visit SM Supermalls for more details</a>
+                </div>
                 <p class="text-center text-white mt-4">
+                    <strong>#SMSuperTikTok</strong>
+                </p>
+                <p class="text-center text-white mt-1">
                     <strong>
-                        Visit www.smsupermalls.com for more details. <br>
                         <small>
                             Per DTI Fair Trade Permit No. FTEB - xxxx, Series of 2021
                         </small>
                     </strong>
+                </p>
+                <p class="text-center mt-4">
+                    <img src="<?= base_url('images/v3/sm-super-month.png') ?>" width="60px">
                 </p>
             </div>
         </div>
